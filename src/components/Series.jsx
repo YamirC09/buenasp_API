b788433340a7729a7f9d4ede62b79a1a49973531
Yamir Coronado <yamirc09@gmail.com>
2023-06-01 21:28:52 -0500

    Commit tres

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 const Series = () => {
+  const [series, setSeries] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetchSeries();
+  }, [currentPage]);
+  
   return (
     <div>Series</div>
   )
