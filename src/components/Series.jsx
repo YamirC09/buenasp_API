5677912004f21b86290aec074c0df4e60b8980e6
Yamir Coronado <yamirc09@gmail.com>
2023-06-01 21:45:25 -0500

    Commit cuatro

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -7,7 +7,17 @@ const Series = () => {
   useEffect(() => {
     fetchSeries();
   }, [currentPage]);
-  
+
+  const fetchSeries = async () => {
+    const apiKey = 'a4d59c5704cd70b188a6dc485879f63a';
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/series?ts=1&apikey=${apiKey}&hash=e831972e0007719d4a7d0d8a4c71f556&limit=10&offset=${(currentPage - 1) * 10}`
+    );
+    const data = await response.json();
+    setSeries(data.data.results);
+  };
+
+
   return (
     <div>Series</div>
   )
